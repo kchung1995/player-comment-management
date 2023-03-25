ba36d41ed86245970a771b7443d863fec4730b1f
Kuyho Chung <kchung1995@gmail.com>
2023-03-26 01:26:45 +0900

    Initial commit

---
+++ src/main/kotlin/com/tistory/katfun/playercommentmanagement/PlayerCommentManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.tistory.katfun.playercommentmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlayerCommentManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlayerCommentManagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/tistory/katfun/playercommentmanagement/PlayerCommentManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.katfun.playercommentmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlayerCommentManagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
