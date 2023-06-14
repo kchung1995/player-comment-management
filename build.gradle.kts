3934bab1aca4d67dc473aa34e09fbed6b0d28913
Kuyho Chung <kchung1995@gmail.com>
2023-06-14 22:39:55 +0900

    feat: added ktlint-gradle dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.11-SNAPSHOT"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
@@ -23,6 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+//    implementation("org.jlleitschuh.gradle:ktlint-gradle:11.4.0")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
